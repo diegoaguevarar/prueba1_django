bb25ed07939d98964f416f825b7c3392b3c5f364
Unknown <diaguevararo@unal.edu.co>
2017-09-22 17:25:48 -0500

    a001

--- djmsc/urls.py
+++ djmsc/urls.py
@@ -7,6 +7,8 @@ from api import views
 router = routers.DefaultRouter()
 router.register(r"event", views.EventViewSet)
 router.register(r"person", views.PersonViewSet)
+router.register(r"usuario", views.UsuarioViewSet)
+router.register(r"transaccion", views.TransaccionViewSet)
 
 
 urlpatterns = [
